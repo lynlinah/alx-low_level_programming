8c10f1075d92242d3598efd95c771b92da77c6f4
lynlinah <carolinanyakio12@gmail.com>
2023-02-20 03:04:41 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-*main - Program to print alphabet letter followed by new line
+*main - Prints the last digit of random number
 *
-*Return: return 0
+*Return: Return 0
 */
 int main(void)
 {
-	int letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (last == 0)
 	{
-		letter = tolower(letter);
-		putchar(letter);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	putchar('\n');
-	return(0);
+	else
+	{
+		if (last > 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, last);
+		}
+		else if (last < 6)
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		}
+	}
+	return (0);
 }
