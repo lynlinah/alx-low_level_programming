f8e42a114d23d69305294a0c638427caa6eb1be4
lynlinah <carolinanyakio12@gmail.com>
2023-02-27 01:45:12 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset the value of an intenger to 98
+ *
+ * @n: Parameter targeted
+ *
+ * Return: nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
