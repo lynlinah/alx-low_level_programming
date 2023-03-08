f972c2644628068d99477cdb05ba4e952325fa58
lynlinah <carolinanyakio12@gmail.com>
2023-03-08 02:52:56 -0800

    101-wildcmp.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - function prints each character
- * @s: targeted output
- * Return: returns void
- */
-void _puts_recursion(char *s);
+* _puts_recursion - prints a string followed by a new line
+*@s:character to be printed
+*Return:returns void
+*/
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		_putchar('\n');
-		return;
+	else
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
 }
