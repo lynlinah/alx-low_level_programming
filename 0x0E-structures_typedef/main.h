331e6d87f9c5289c2396e14c5d27ec72e2dd653d
lynlinah <carolinanyakio12@gmail.com>
2023-03-20 10:56:21 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * char prototypes
+ * float prototypes
+ */
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
